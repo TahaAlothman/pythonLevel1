e14a4be199d03bbf41e82c917d80326b7ccce941
[TahaAlothman] <tahaaborawi@gmail.com>
2023-06-19 01:10:08 +0200

    add Data-Types_Task

---
+++ Data_Types_Task.py
@@ -0,0 +1,59 @@
+#2.Create a string with the name ‘mystero’
+Name = 'mystro'
+
+#3.Make the string letters capital
+Name = Name.upper()
+print(Name)
+
+#4 Create a list of values from 10 to 20
+
+List = [10,11,12,13,14,15,16,17,18,19,20]
+
+#5.Add 30 to the end of the list
+
+List.append(30)
+
+print(List)
+
+#6.Add 40 as the second element of the list
+
+List.insert(1,40)
+print(List)
+
+
+#7.Print how many elements in the list
+
+print (len(List))
+
+#8.replace the second element in the list with 100
+List[1] = 100
+print (List)
+
+
+#9.Create a tuple with values from 1 to 5
+
+Tuble = (1,2,3,4,5)
+
+#10.Can we add 10 to the end of the tuple?
+
+print("We can't edit the elements because the tuble elements are constant")
+
+
+#11.Create a dict of value Mahmoud:28 , ahmed:30
+
+Dict ={'Mahmoud':28 , 'ahmed':30}
+
+#12.Print Mahmoud age from the dict
+
+print(Dict['Mahmoud'])
+
+#13.What is the differences between mutable and immutable data types ?
+
+print("In Python, an object whose internal state can be changed is mutable, while an immutable type doesn't allow any change in the object once it has been created")
+
+
+
+
+
+
+
